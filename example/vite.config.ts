755d784c312c308920f89f85e50c91ca902f1ac9
Windmill_City <1449182174@qq.com>
2026-01-28 09:58:36 +0800

    dev同时运行监听和vite

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -4,11 +4,6 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    watch: {
-      ignored: ["!**/lib/**"],
-    },
-  },
   optimizeDeps: {
     exclude: ["@local/resizable-panels"],
   },
