3fa104599fc7c05ba7f77ca25b85c261eb64ef03
Windmill_City <1449182174@qq.com>
2026-01-29 13:59:35 +0800

    修正box模型

--- example/src/ui/menu-bar.tsx
+++ example/src/ui/menu-bar.tsx
@@ -1,5 +1,5 @@
 const MenuBar = ({ children }: { children?: React.ReactNode }) => {
-  return <div className="h-8 flex relative border-b">{children}</div>
+  return <div className="h-8 flex box-content relative border-b">{children}</div>
 }
 
 export default MenuBar
--- example/src/ui/status-bar.tsx
+++ example/src/ui/status-bar.tsx
@@ -1,5 +1,5 @@
 const StatusBar = ({ children }: { children?: React.ReactNode }) => {
-  return <div className="h-6 flex border-t overflow-hidden">{children}</div>
+  return <div className="h-6 flex box-content border-t">{children}</div>
 }
 
 export default StatusBar
