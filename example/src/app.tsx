40d1a825f05b03aea065ce5ff007a4ca4cfb516b
Windmill_City <1449182174@qq.com>
2026-02-01 00:13:04 +0800

    移除无用maxSize

--- example/src/app.tsx
+++ example/src/app.tsx
@@ -21,7 +21,7 @@ function App() {
             >
               left
             </ResizablePanel>
-            <ResizablePanel expand maxSize={1600}>
+            <ResizablePanel expand>
               <ResizableGroup direction="row">
                 {/* Editor */}
                 <ResizablePanel minSize={80} collapsible expand>
