4abfc92571b974fa571557dd524c36367b7e25d3
Windmill_City <1449182174@qq.com>
2026-01-31 18:41:40 +0800

    添加部署任务

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/resizable-panels/",
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
     exclude: ["@local/resizable-panels"],
