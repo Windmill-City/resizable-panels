ffa8e57cc1f4590a1b6db5057a88d729ccd80365
Windmill_City <1449182174@qq.com>
2026-02-01 12:55:13 +0800

    修正export

--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,27 @@
-export { ResizableContext, useResizableContext } from "./context"
-export { ResizableGroup, useGroupContext } from "./group"
+export {
+  adjustPanelByDelta,
+  findEdgeIndexAtPoint,
+  growSequentially,
+  HANDLE_SIZE,
+  ResizableContext,
+  ResizableContextType,
+  shrinkSequentially,
+  useResizableContext,
+  WINDOW_EDGE_MARGIN
+} from "./context"
+export { GroupContext, ResizableGroup, useGroupContext } from "./group"
 export { ResizableHandle } from "./handle"
 export { ResizablePanel } from "./panel"
 
 export type {
   ContextValue,
+  Direction,
   GroupValue,
   HandleValue,
-  Direction,
   PanelValue,
   ResizableContextProps,
   ResizableGroupProps,
-  ResizablePanelProps,
   ResizableHandleProps,
+  ResizablePanelProps
 } from "./types"
+
