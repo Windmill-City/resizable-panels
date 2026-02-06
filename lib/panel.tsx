272d2e7a295f7114591b0eecbfb0a9e9069f8ca3
Windmill_City <1449182174@qq.com>
2026-02-06 14:12:39 +0800

    删除无用属性

--- lib/panel.tsx
+++ lib/panel.tsx
@@ -119,7 +119,6 @@ export function ResizablePanel({
       <div
         ref={containerEl}
         data-resizable-panel={ref.id}
-        data-direction={group.direction}
         data-collapsed={ref.isCollapsed || undefined}
         data-maximized={ref.isMaximized || undefined}
         style={{
