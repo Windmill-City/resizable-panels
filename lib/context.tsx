1310c017c5a84b13af3a1f237692301f6c4211c4
Windmill_City <1449182174@qq.com>
2026-02-11 10:06:02 +0800

    Revert "鼠标抬起时执行一次notify"
    This reverts commit ba1a680674706e34888452a36eac0a87ce616e74.

--- lib/context.tsx
+++ lib/context.tsx
@@ -590,7 +590,6 @@ export function ResizableContext({
       // Reset drag state
       ref.isDragging = false
       ref.dragIndex = []
-      ref.notify()
 
       console.debug("[Context] DragEnd")
 
